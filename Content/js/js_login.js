e33d41c1f43b8c78daa38288a4d573bcacc0f920
Tsần Quý Viên <quyviensv1@gmail.com>
2025-11-03 22:05:02 +0700

    ;;;

--- Content/js/js_login.js
+++ Content/js/js_login.js
@@ -31,6 +31,9 @@ document.getElementById("formLogin").addEventListener("submit", function (e) {
 
 
 
+
+
+
     if (valid) {
         alert("Đăng nhập thành công!");
         window.location.href = "/Default/Index";
