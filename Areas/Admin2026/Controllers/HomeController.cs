5d6bee669abf36516e9ca9b30c98a5c35620b2c8
kiet <mkla1252006@gmail.com>
2025-11-04 19:17:45 +0700

    thay doi 4/11

---
+++ Areas/Admin2026/Controllers/HomeController.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace DoAn_web.Areas.Admin2026.Controllers
+{
+    public class HomeController : Controller
+    {
+        // GET: Admin2026/Home
+        public ActionResult Index()
+        {
+            return View();
+        }
+    }
+}
