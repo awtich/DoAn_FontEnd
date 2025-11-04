56f4875ff7c28c3517053a937c06647de540fbfb
awtich <mkla1252006@gmail.com>
2025-11-04 19:24:38 +0700

    Baitap (#11)
    * ddd
    
    * f
    
    * 454455
    
    * Sửa login
    
    * lll
    
    * ;;;
    
    * ;;;
    
    * thay doi 4/11
    
    ---------
    
    Co-authored-by: Tsần Quý Viên <quyviensv1@gmail.com>

---
+++ Areas/Admin2026/Controllers/ProductsController.cs
@@ -0,0 +1,154 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Data;
+using System.Data.Entity;
+using System.Linq;
+using System.Net;
+using System.Web;
+using System.Web.Mvc;
+using DoAn_web.Models;
+using System.IO;
+
+namespace DoAn_web.Areas.Admin2026.Controllers
+{
+    public class ProductsController : Controller
+    {
+        private MyStore2026Entities db = new MyStore2026Entities();
+
+        // GET: Admin2026/Products
+        public ActionResult Index()
+        {
+            var products = db.Products.Include(p => p.Category);
+            return View(products.ToList());
+        }
+
+        // GET: Admin2026/Products/Details/5
+        public ActionResult Details(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // GET: Admin2026/Products/Create
+        public ActionResult Create()
+        {
+            // Gửi danh sách Category cho DropDownList
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName");
+            return View();
+        }
+
+        // POST: Admin2026/Products/Create
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Create([Bind(Include = "ProductID,ProductName,ProductDecription,ProductPrice,CategoryID")] Product product, HttpPostedFileBase ImageFile)
+        {
+            // Xử lý file được upload
+            if (ImageFile != null && ImageFile.ContentLength > 0)
+            {
+                // Lấy tên file
+                var fileName = Path.GetFileName(ImageFile.FileName);
+
+                // Tạo đường dẫn lưu file (bạn PHẢI tạo thư mục này)
+                var serverPath = Path.Combine(Server.MapPath("~/Content/Images/Products/"), fileName);
+
+                // Lưu file vào server
+                ImageFile.SaveAs(serverPath);
+                
+                // Lưu đường dẫn vào database
+                product.ProductImage = "~/Content/Images/Products/" + fileName;
+            }
+            else
+            {
+                // (Tùy chọn) Gán một ảnh mặc định nếu không upload
+                // product.ProductImage = "~/Content/Images/default.jpg";
+            }
+
+            if (ModelState.IsValid)
+            {
+                db.Products.Add(product);
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+
+            // Nếu lỗi, gửi lại DropDownList
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // GET: Admin2026/Products/Edit/5
+        public ActionResult Edit(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // POST: Admin2026/Products/Edit/5
+        // To protect from overposting attacks, enable the specific properties you want to bind to, for 
+        // more details see https://go.microsoft.com/fwlink/?LinkId=317598.
+        [HttpPost]
+        [ValidateAntiForgeryToken]
+        public ActionResult Edit([Bind(Include = "ProductID,CategoryID,ProductName,ProductDecription,ProductPrice,ProductImage")] Product product)
+        {
+            if (ModelState.IsValid)
+            {
+                db.Entry(product).State = System.Data.Entity.EntityState.Modified;
+                db.SaveChanges();
+                return RedirectToAction("Index");
+            }
+            ViewBag.CategoryID = new SelectList(db.Categories, "CategoryID", "CategoryName", product.CategoryID);
+            return View(product);
+        }
+
+        // GET: Admin2026/Products/Delete/5
+        public ActionResult Delete(int? id)
+        {
+            if (id == null)
+            {
+                return new HttpStatusCodeResult(HttpStatusCode.BadRequest);
+            }
+            Product product = db.Products.Find(id);
+            if (product == null)
+            {
+                return HttpNotFound();
+            }
+            return View(product);
+        }
+
+        // POST: Admin2026/Products/Delete/5
+        [HttpPost, ActionName("Delete")]
+        [ValidateAntiForgeryToken]
+        public ActionResult DeleteConfirmed(int id)
+        {
+            Product product = db.Products.Find(id);
+            db.Products.Remove(product);
+            db.SaveChanges();
+            return RedirectToAction("Index");
+        }
+
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing)
+            {
+                db.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+    }
+}
