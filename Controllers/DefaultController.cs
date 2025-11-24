5137895324749d1559990b59aeb87a8f791d3fba
Admin <Admin@DESKTOP-DUSL2G4>
2025-11-24 23:08:59 +0700

    Lich sử đơn hàng 24/11 23h

--- Controllers/DefaultController.cs
+++ Controllers/DefaultController.cs
@@ -51,11 +51,11 @@ namespace DoAn_web.Controllers
                 .ToList();
             // 4 sp watch
             viewModel.PhuKiens = db.Products
-    .Include(p => p.Category)
-    .Where(p => p.Category.CategoryName == "PhuKien")// loc
-    .OrderByDescending(p => p.ProductID)// lay sp moi nhat
-    .Take(4)// lay 4 sp
-    .ToList();
+            .Include(p => p.Category)
+            .Where(p => p.Category.CategoryName == "PhuKien")// loc
+            .OrderByDescending(p => p.ProductID)// lay sp moi nhat
+            .Take(4)// lay 4 sp
+            .ToList();
             //6 gui du lieu ve View
             return View(viewModel);
 
@@ -97,6 +97,31 @@ namespace DoAn_web.Controllers
             int customerID = GetCurrentCustomerID(); // (Copy hàm này từ CartController sang hoặc gom vào BaseController)
             var orders = db.Orders.Where(o => o.CustomerID == customerID).OrderByDescending(o => o.OrderDate).ToList();
             return View(orders);
+        }
+        public ActionResult CancelOrder(int id)
+        {
+            //Lấy id của người dùng đang đăng nhập
+            int customerId = GetCurrentCustomerID();
+            //Truy vấn db tìm cái đầu tiên nếu k thì trả về null
+            var order = db.Orders.FirstOrDefault(o => o.OrderID == id && o.CustomerID == customerId);
+            if (order != null && ((order.PaymentStatus == "Chờ xác nhận chuyển khoản" ||
+                                                    order.PaymentStatus == "Chờ xử lý" ||
+                                                    order.PaymentStatus == "Chưa thanh toán")) && order.ShippingStatus != "Đang vận chuyển" && order.ShippingStatus != "Đã giao thành công")
+            {
+                order.PaymentStatus = "Đã Huỷ !";
+                order.ShippingStatus = "Đã Huỷ !";
+
+                db.SaveChanges();
+                //Cập nhật 
+                TempData["SuccessMessage"] = "Order #" + id + "has been cancelled !";
+            }
+            else
+            {
+                TempData["ErrorMessage"] = "Cannot cancel this order !";
+            }
+            return RedirectToAction("OrderHistory");
+                
+
         }
         
 
